3c969194da9028bd6ee7b33b3d426febc59b873c
Marco Seguri <seguri@users.noreply.github.com>
2024-12-18 20:18:29 +0100

    Tests for version 1.0.2

---
+++ JohnnyDecimalIdTests.cs
@@ -0,0 +1,74 @@
+﻿namespace Community.PowerToys.Run.Plugin.JohnnyDecimal.Tests
+{
+    [TestClass()]
+    public class JohnnyDecimalIdTests
+    {
+        [TestMethod()]
+        public void NullQuery_TryParse_fail()
+        {
+            string query = null;
+
+            var isParsed = JohnnyDecimalId.TryParse(query, out var result);
+
+            Assert.IsFalse(isParsed);
+            Assert.IsNotNull(result);
+            Assert.IsTrue(string.IsNullOrEmpty(result.GetCategory()));
+            Assert.IsTrue(string.IsNullOrEmpty(result.GetId()));
+        }
+
+        [TestMethod()]
+        public void EmptyQuery_TryParse_fail()
+        {
+            string query = "";
+
+            var isParsed = JohnnyDecimalId.TryParse(query, out var result);
+
+            Assert.IsFalse(isParsed);
+            Assert.IsNotNull(result);
+            Assert.IsTrue(string.IsNullOrEmpty(result.GetCategory()));
+            Assert.IsTrue(string.IsNullOrEmpty(result.GetId()));
+        }
+
+        [TestMethod()]
+        public void ValidQuery1_TryParse_success()
+        {
+            var query = "1234 Foo";
+
+            var isParsed = JohnnyDecimalId.TryParse(query, out var result);
+
+            Assert.IsTrue(isParsed);
+            Assert.IsNotNull(result);
+            Assert.AreEqual('1', result.GetArea());
+            Assert.AreEqual("12", result.GetCategory());
+            Assert.AreEqual("34", result.GetId());
+        }
+
+        [TestMethod()]
+        public void ValidQuery2_TryParse_success()
+        {
+            var query = "12.34 Foo";
+
+            var isParsed = JohnnyDecimalId.TryParse(query, out var result);
+
+            Assert.IsTrue(isParsed);
+            Assert.IsNotNull(result);
+            Assert.AreEqual('1', result.GetArea());
+            Assert.AreEqual("12", result.GetCategory());
+            Assert.AreEqual("34", result.GetId());
+        }
+
+        [TestMethod()]
+        public void ValidQuery3_TryParse_success()
+        {
+            string query = "123456789";
+
+            var isParsed = JohnnyDecimalId.TryParse(query, out var result);
+
+            Assert.IsTrue(isParsed);
+            Assert.IsNotNull(result);
+            Assert.AreEqual('1', result.GetArea());
+            Assert.AreEqual("12", result.GetCategory());
+            Assert.AreEqual("34", result.GetId());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# PowerToys-Run-JohnnyDecimalTests
+
+The tests of [PowerToys-Run-JohnnyDecimal](https://github.com/seguri/PowerToys-Run-JohnnyDecimal).
+

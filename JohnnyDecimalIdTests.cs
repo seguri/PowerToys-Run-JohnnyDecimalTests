d9d9a789154a134d0ebc96c7e09b919338c35832
Marco Seguri <seguri@users.noreply.github.com>
2025-01-08 18:14:58 +0100

    Make JohnnyDecimalID.cs parse partial IDs

--- JohnnyDecimalIdTests.cs
+++ JohnnyDecimalIdTests.cs
@@ -12,8 +12,12 @@
 
             Assert.IsFalse(isParsed);
             Assert.IsNotNull(result);
-            Assert.IsTrue(string.IsNullOrEmpty(result.GetCategory()));
-            Assert.IsTrue(string.IsNullOrEmpty(result.GetId()));
+            Assert.IsTrue(string.IsNullOrEmpty(result.Area));
+            Assert.AreEqual("", result.AreaGlob);
+            Assert.IsTrue(string.IsNullOrEmpty(result.Category));
+            Assert.AreEqual("", result.CategoryGlob);
+            Assert.IsTrue(string.IsNullOrEmpty(result.Id));
+            Assert.AreEqual("", result.IdGlob);
         }
 
         [TestMethod()]
@@ -25,12 +29,33 @@
 
             Assert.IsFalse(isParsed);
             Assert.IsNotNull(result);
-            Assert.IsTrue(string.IsNullOrEmpty(result.GetCategory()));
-            Assert.IsTrue(string.IsNullOrEmpty(result.GetId()));
+            Assert.IsTrue(string.IsNullOrEmpty(result.Area));
+            Assert.AreEqual("", result.AreaGlob);
+            Assert.IsTrue(string.IsNullOrEmpty(result.Category));
+            Assert.AreEqual("", result.CategoryGlob);
+            Assert.IsTrue(string.IsNullOrEmpty(result.Id));
+            Assert.AreEqual("", result.IdGlob);
         }
 
         [TestMethod()]
-        public void ValidQuery1_TryParse_success()
+        public void PartialId1_TryParse_success()
+        {
+            var query = "123";
+
+            var isParsed = JohnnyDecimalId.TryParse(query, out var result);
+
+            Assert.IsTrue(isParsed);
+            Assert.IsNotNull(result);
+            Assert.AreEqual("1", result.Area);
+            Assert.AreEqual("10-19*", result.AreaGlob);
+            Assert.AreEqual("12", result.Category);
+            Assert.AreEqual("12*", result.CategoryGlob);
+            Assert.AreEqual("3", result.Id);
+            Assert.AreEqual("12.3*", result.IdGlob);
+        }
+
+        [TestMethod()]
+        public void ValidId1_TryParse_success()
         {
             var query = "1234 Foo";
 
@@ -38,13 +63,16 @@
 
             Assert.IsTrue(isParsed);
             Assert.IsNotNull(result);
-            Assert.AreEqual('1', result.GetArea());
-            Assert.AreEqual("12", result.GetCategory());
-            Assert.AreEqual("34", result.GetId());
+            Assert.AreEqual("1", result.Area);
+            Assert.AreEqual("10-19*", result.AreaGlob);
+            Assert.AreEqual("12", result.Category);
+            Assert.AreEqual("12*", result.CategoryGlob);
+            Assert.AreEqual("34", result.Id);
+            Assert.AreEqual("12.34*", result.IdGlob);
         }
 
         [TestMethod()]
-        public void ValidQuery2_TryParse_success()
+        public void ValidId2_TryParse_success()
         {
             var query = "12.34 Foo";
 
@@ -52,13 +80,16 @@
 
             Assert.IsTrue(isParsed);
             Assert.IsNotNull(result);
-            Assert.AreEqual('1', result.GetArea());
-            Assert.AreEqual("12", result.GetCategory());
-            Assert.AreEqual("34", result.GetId());
+            Assert.AreEqual("1", result.Area);
+            Assert.AreEqual("10-19*", result.AreaGlob);
+            Assert.AreEqual("12", result.Category);
+            Assert.AreEqual("12*", result.CategoryGlob);
+            Assert.AreEqual("34", result.Id);
+            Assert.AreEqual("12.34*", result.IdGlob);
         }
 
         [TestMethod()]
-        public void ValidQuery3_TryParse_success()
+        public void ValidId3_TryParse_success()
         {
             string query = "123456789";
 
@@ -66,9 +97,80 @@
 
             Assert.IsTrue(isParsed);
             Assert.IsNotNull(result);
-            Assert.AreEqual('1', result.GetArea());
-            Assert.AreEqual("12", result.GetCategory());
-            Assert.AreEqual("34", result.GetId());
+            Assert.AreEqual("1", result.Area);
+            Assert.AreEqual("10-19*", result.AreaGlob);
+            Assert.AreEqual("12", result.Category);
+            Assert.AreEqual("12*", result.CategoryGlob);
+            Assert.AreEqual("34", result.Id);
+            Assert.AreEqual("12.34*", result.IdGlob);
+        }
+
+        [TestMethod()]
+        public void ValidCat1_TryParse_success()
+        {
+            var query = "12";
+
+            var isParsed = JohnnyDecimalId.TryParse(query, out var result);
+
+            Assert.IsTrue(isParsed);
+            Assert.IsNotNull(result);
+            Assert.AreEqual("1", result.Area);
+            Assert.AreEqual("10-19*", result.AreaGlob);
+            Assert.AreEqual("12", result.Category);
+            Assert.AreEqual("12*", result.CategoryGlob);
+            Assert.IsTrue(string.IsNullOrEmpty(result.Id));
+            Assert.IsTrue(string.IsNullOrEmpty(result.IdGlob));
+        }
+
+        [TestMethod()]
+        public void ValidCat2_TryParse_success()
+        {
+            var query = "12foo";
+
+            var isParsed = JohnnyDecimalId.TryParse(query, out var result);
+
+            Assert.IsTrue(isParsed);
+            Assert.IsNotNull(result);
+            Assert.AreEqual("1", result.Area);
+            Assert.AreEqual("10-19*", result.AreaGlob);
+            Assert.AreEqual("12", result.Category);
+            Assert.AreEqual("12*", result.CategoryGlob);
+            Assert.IsTrue(string.IsNullOrEmpty(result.Id));
+            Assert.IsTrue(string.IsNullOrEmpty(result.IdGlob));
+        }
+
+        [TestMethod()]
+        public void ValidArea1_TryParse_success()
+        {
+            var query = "1";
+
+            var isParsed = JohnnyDecimalId.TryParse(query, out var result);
+
+            Assert.IsTrue(isParsed);
+            Assert.IsNotNull(result);
+            Assert.AreEqual("1", result.Area);
+            Assert.AreEqual("10-19*", result.AreaGlob);
+            Assert.IsTrue(string.IsNullOrEmpty(result.Category));
+            Assert.IsTrue(string.IsNullOrEmpty(result.CategoryGlob));
+            Assert.IsTrue(string.IsNullOrEmpty(result.Id));
+            Assert.IsTrue(string.IsNullOrEmpty(result.IdGlob));
+        }
+
+        [TestMethod()]
+        public void ValidArea2_TryParse_success()
+        {
+            var query = "1foo";
+
+            var isParsed = JohnnyDecimalId.TryParse(query, out var result);
+
+            Assert.IsTrue(isParsed);
+            Assert.IsNotNull(result);
+            Assert.AreEqual("1", result.Area);
+            Assert.AreEqual("10-19*", result.AreaGlob);
+            Assert.IsTrue(string.IsNullOrEmpty(result.Category));
+            Assert.IsTrue(string.IsNullOrEmpty(result.CategoryGlob));
+            Assert.IsTrue(string.IsNullOrEmpty(result.Id));
+            Assert.IsTrue(string.IsNullOrEmpty(result.IdGlob));
         }
     }
 }
